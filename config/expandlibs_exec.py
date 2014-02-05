18fab4d574fa44bc4b818382d847885d6a3ac87e
Mike Perry <mikeperry-git@torproject.org>
2014-02-05 14:15:42 -0800

    Bug #10159: Build determinism fixes
    Enforce sorted ordering on build and link lists. Also omit hostname from
    buildinfo.

--- config/expandlibs_exec.py
+++ config/expandlibs_exec.py
@@ -330,7 +330,7 @@ def main():
         if options.uselist:
             args.makelist()
 
-        if options.verbose:
+        if True: #options.verbose:
             print_command(sys.stderr, args)
         try:
             proc = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
