90f7fdf8f4f2abad2573531aeb1043257c2909f8
Arthur Edelstein <arthuredelstein@gmail.com>
2015-03-25 12:07:56 -0700

    fixup! Bug #5926: Allow customization of content JS locale.

--- xpcom/build/nsXPComInit.cpp
+++ xpcom/build/nsXPComInit.cpp
@@ -478,7 +478,7 @@ void UseUSEnglishLocalePrefChangedCallback(const char* /* pref */, void* /* clos
     // Read the pref to see if we will use US English locale.
     bool useUSEnglishLocale = mozilla::Preferences::GetBool(USE_US_ENGLISH_LOCALE_PREF, false);
     // Set the application-wide C-locale. Needed for Date.toLocaleFormat().
-    setlocale(LC_ALL, useUSEnglishLocale ? "en_US" : sSystemLocale);
+    setlocale(LC_ALL, useUSEnglishLocale ? "C" : sSystemLocale);
     // Now override the JavaScript Runtime Locale that is used by the Intl API
     // as well as Date.toLocaleString, Number.toLocaleString, and String.localeCompare.
     JS_SetDefaultLocale(rt, useUSEnglishLocale ? "en-US" : sJSLocale);
