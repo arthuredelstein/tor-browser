8e679519de3ee3928d3e3f1b4ccb754066d5b7b2
Mike Perry <mikeperry-git@torproject.org>
2015-04-21 14:13:19 -0700

    Bug 1517: Reduce precision of time for Javascript.
     This clamps Javascript's time precision to 100ms for most things, without
     altering event delivery or responsiveness. It also clamps keyboard events
     (such as keypress, keyup, keydown, etc) to 250ms resolution, to mitigate
     keystroke fingerprinting.

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -1734,6 +1734,9 @@ private:
   // be seeked even before the media is loaded.
   double mDefaultPlaybackStartPosition;
 
+  // The unrounded current time
+  double CurrentTimeImpl() const;
+
   // True if the audio track is not silent.
   bool mIsAudioTrackAudible;
 
