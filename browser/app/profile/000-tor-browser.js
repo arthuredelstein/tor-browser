ab2350bbe41ad1453466a785817df2adcf0ebbbe
Mike Perry <mikeperry-git@torproject.org>
2015-07-06 20:28:27 -0700

    fixup! TB4: Tor Browser's Firefox preference overrides.
    Bug #16316: Set the fetch URLs to blank data URIs.

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -58,7 +58,11 @@ pref("services.sync.engine.prefs", false); // Never sync prefs, addons, or tabs 
 pref("services.sync.engine.addons", false);
 pref("services.sync.engine.tabs", false);
 pref("extensions.getAddons.cache.enabled", false); // https://blog.mozilla.org/addons/how-to-opt-out-of-add-on-metadata-updates/
+pref("browser.newtabpage.directory.ping", "data:text/plain,"); // Bug 16316 - Avoid potential confusion over tiles for now.
+pref("browser.newtabpage.directory.source", "data:text/plain,"); // Bug 16316 - Avoid potential confusion over tiles for now.
 pref("browser.newtabpage.enhanced", false); // Bug 16316 - Avoid potential confusion over tiles for now.
+pref("browser.newtabpage.introShown", true); // Bug 16316 - Avoid potential confusion over tiles for now.
+pref("browser.newtabpage.preload", false); // Bug 16316 - Avoid potential confusion over tiles for now.
 pref("browser.search.countryCode", "US"); // The next three prefs disable GeoIP search lookups (#16254)
 pref("browser.search.region", "US");
 pref("browser.search.geoip.url", "");
