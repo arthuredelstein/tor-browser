7fab8c4fc5dbbff2110045c5d2531c94cb15eea4
Arthur Edelstein <arthuredelstein@gmail.com>
2015-09-07 22:49:25 -0700

    fixup! Bug 15646: Prevent keyboard layout fingerprinting in KeyboardEvent

--- dom/events/KeyboardEvent.cpp
+++ dom/events/KeyboardEvent.cpp
@@ -12,11 +12,6 @@
 namespace mozilla {
 namespace dom {
 
-static bool ResistFingerprinting() {
-  return nsContentUtils::ResistFingerprinting() &&
-         !nsContentUtils::ThreadsafeIsCallerChrome();
-}
-
 KeyboardEvent::KeyboardEvent(EventTarget* aOwner,
                              nsPresContext* aPresContext,
                              WidgetKeyboardEvent* aEvent)
@@ -42,6 +37,12 @@ NS_IMPL_RELEASE_INHERITED(KeyboardEvent, UIEvent)
 NS_INTERFACE_MAP_BEGIN(KeyboardEvent)
   NS_INTERFACE_MAP_ENTRY(nsIDOMKeyEvent)
 NS_INTERFACE_MAP_END_INHERITING(UIEvent)
+
+bool
+KeyboardEvent::ResistFingerprinting() {
+  return nsContentUtils::ResistFingerprinting() &&
+         !nsContentUtils::ThreadsafeIsCallerChrome();
+}
 
 bool
 KeyboardEvent::AltKey()
