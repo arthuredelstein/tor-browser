9fa9671ed0c69ecda9b2b3aaeca7f832ac5147f5
Arthur Edelstein <arthuredelstein@gmail.com>
2016-01-22 18:37:47 -0800

    Bug 1121643 - Add "font.system.whitelist" pref to resist font-based fingerprinting

--- gfx/thebes/gfxDWriteFontList.cpp
+++ gfx/thebes/gfxDWriteFontList.cpp
@@ -733,7 +733,7 @@ gfxDWriteFontList::gfxDWriteFontList()
 //   Arial to avoid this.
 
 gfxFontFamily *
-gfxDWriteFontList::GetDefaultFont(const gfxFontStyle *aStyle)
+gfxDWriteFontList::GetDefaultFontForPlatform(const gfxFontStyle *aStyle)
 {
     nsAutoString resolvedName;
 
@@ -849,7 +849,7 @@ enum DWriteInitError {
 };
 
 nsresult
-gfxDWriteFontList::InitFontList()
+gfxDWriteFontList::InitFontListForPlatform()
 {
     LARGE_INTEGER frequency;          // ticks per second
     LARGE_INTEGER t1, t2, t3, t4, t5; // ticks
@@ -869,8 +869,6 @@ gfxDWriteFontList::InitFontList()
     mGDIFontTableAccess =
         Preferences::GetBool("gfx.font_rendering.directwrite.use_gdi_table_loading",
                              false);
-
-    gfxPlatformFontList::InitFontList();
 
     mFontSubstitutes.Clear();
     mNonExistingFonts.Clear();
@@ -1403,7 +1401,8 @@ gfxDWriteFontList::GlobalFontFallback(const uint32_t aCh,
                                       uint32_t& aCmapCount,
                                       gfxFontFamily** aMatchedFamily)
 {
-    bool useCmaps = gfxPlatform::GetPlatform()->UseCmapsDuringSystemFallback();
+    bool useCmaps = IsFontFamilyWhitelistActive() ||
+                    gfxPlatform::GetPlatform()->UseCmapsDuringSystemFallback();
 
     if (useCmaps) {
         return gfxPlatformFontList::GlobalFontFallback(aCh,
