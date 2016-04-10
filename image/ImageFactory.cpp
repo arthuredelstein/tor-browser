f3584b6dc11f7b76ddae72af2ebacf23198d5258
Kathy Brade <brade@pearlcrescent.com>
2016-04-10 08:34:54 -0700

    squash! Bug 12827: Create preference to disable SVG.
    Also disallow use of SVG in favicons (bug 18602).
    
    The browser does some negative caching of failed favicon loads, so
    SVG favicons may not begin to work on all pages immediately after
    svg.in-content.enabled is changed from false to true.

--- image/ImageFactory.cpp
+++ image/ImageFactory.cpp
@@ -78,16 +78,6 @@ ComputeImageFlags(ImageURL* uri, const nsCString& aMimeType, bool isMultiPart)
   return imageFlags;
 }
 
-// Marks an image as having an error before returning it.
-template <typename T>
-static already_AddRefed<Image>
-BadImage(const char* aMessage, RefPtr<T>& aImage)
-{
-  NS_WARNING(aMessage);
-  aImage->SetHasError();
-  return aImage.forget();
-}
-
 /* static */ already_AddRefed<Image>
 ImageFactory::CreateImage(nsIRequest* aRequest,
                           ProgressTracker* aProgressTracker,
@@ -103,15 +93,41 @@ ImageFactory::CreateImage(nsIRequest* aRequest,
   uint32_t imageFlags = ComputeImageFlags(aURI, aMimeType, aIsMultiPart);
 
   // Select the type of image to create based on MIME type.
+  bool isBlocked = false;
   if (aMimeType.EqualsLiteral(IMAGE_SVG_XML)) {
     nsCOMPtr<nsIChannel> channel(do_QueryInterface(aRequest));
-    if (!NS_SVGEnabledForChannel(channel)) {
+    isBlocked = !NS_SVGEnabledForChannel(channel);
+    if (!isBlocked && channel && !NS_SVGEnabledForChannel(nullptr)) {
+      // Special case for favicons: block this image load if SVGs are
+      // disallowed from content and if this image load originated from the
+      // favicon xul:image node within a tab.
+      nsCOMPtr<nsILoadInfo> loadInfo;
+      nsresult rv = channel->GetLoadInfo(getter_AddRefs(loadInfo));
+      if (NS_SUCCEEDED(rv)) {
+        nsINode *node = loadInfo->LoadingNode();
+        if (node) {
+          nsCOMPtr<nsIDOMElement> elem(do_QueryInterface(node));
+          if (elem) {
+            nsAutoString anonid;
+            elem->GetAttribute(NS_LITERAL_STRING("anonid"), anonid);
+            isBlocked = anonid.EqualsLiteral("tab-icon-image");
+          }
+        }
+      }
+    }
+
+    if (isBlocked) {
       // SVG is disabled.  We must return an image object that is marked
       // "bad", but we want to avoid invoking the VectorImage class (SVG code),
       // so we return a PNG with the error flag set.
       RefPtr<RasterImage> badImage = new RasterImage(aURI);
       (void)badImage->Init(IMAGE_PNG, Image::INIT_FLAG_NONE);
-      return BadImage("SVG disabled: ImageFactor::CreateImage failed", badImage);
+      if (aProgressTracker) {
+        aProgressTracker->SetImage(badImage);
+        badImage->SetProgressTracker(aProgressTracker);
+      }
+      badImage->SetHasError();
+      return badImage.forget();
     }
     return CreateVectorImage(aRequest, aProgressTracker, aMimeType,
                              aURI, imageFlags, aInnerWindowId);
@@ -119,6 +135,16 @@ ImageFactory::CreateImage(nsIRequest* aRequest,
     return CreateRasterImage(aRequest, aProgressTracker, aMimeType,
                              aURI, imageFlags, aInnerWindowId);
   }
+}
+
+// Marks an image as having an error before returning it.
+template <typename T>
+static already_AddRefed<Image>
+BadImage(const char* aMessage, RefPtr<T>& aImage)
+{
+  NS_WARNING(aMessage);
+  aImage->SetHasError();
+  return aImage.forget();
 }
 
 /* static */ already_AddRefed<Image>
