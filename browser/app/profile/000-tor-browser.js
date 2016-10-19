62d6468cedbdd1ae08b1e4a7192184c3aa92137c
Arthur Edelstein <arthuredelstein@gmail.com>
2016-10-18 18:10:41 -0700

    fixup! TB4: Tor Browser's Firefox preference overrides.
    Fixes #20399 by removing obsolete preferences related to our font
    fingerprinting defense.

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -112,8 +112,6 @@ pref("webgl.disable-extensions", true);
 pref("webgl.disable-fail-if-major-performance-caveat", true);
 pref("dom.battery.enabled", false); // fingerprinting due to differing OS implementations
 pref("dom.network.enabled",false); // fingerprinting due to differing OS implementations
-pref("browser.display.max_font_attempts",10);
-pref("browser.display.max_font_count",10);
 pref("gfx.downloadable_fonts.fallback_delay", -1);
 pref("general.appname.override", "Netscape");
 pref("general.appversion.override", "5.0 (Windows)");
