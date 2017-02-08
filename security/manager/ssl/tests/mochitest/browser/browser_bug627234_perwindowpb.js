6ea222d386b112ce06931c68aa891968bbcc61a9
Jonathan Hao <jhao@mozilla.com>
2017-02-08 11:17:29 +0800

    Bug 1336867 - Remove unsafeProcessHeader and isSecureHost in nsISiteSecurityService r=keeler,mgoodwin,past
    --HG--
    extra : rebase_source : 3b14ddfcb4107eeeb15ba5498d0b516310847c58
    
    Conflicts:
    	devtools/client/debugger/new/debugger.js
    	security/manager/ssl/tests/unit/test_sts_parser.js
    	services/common/tests/unit/test_blocklist_pinning.js

--- security/manager/ssl/tests/mochitest/browser/browser_bug627234_perwindowpb.js
+++ security/manager/ssl/tests/mochitest/browser/browser_bug627234_perwindowpb.js
@@ -56,7 +56,9 @@ function test() {
       uri = aWindow.Services.io.newURI("https://localhost/img.png");
       gSSService.processHeader(Ci.nsISiteSecurityService.HEADER_HSTS, uri,
                                "max-age=1000", sslStatus, privacyFlags(aIsPrivateMode));
-      ok(gSSService.isSecureHost(Ci.nsISiteSecurityService.HEADER_HSTS, "localhost", privacyFlags(aIsPrivateMode)), "checking sts host");
+      ok(gSSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS, uri,
+                                privacyFlags(aIsPrivateMode)),
+                                "checking sts host");
 
       aCallback();
     }, true);
