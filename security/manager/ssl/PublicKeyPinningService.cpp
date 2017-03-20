Floorp-Projects/Floorp

Bug 1323644 - Isolate the HSTS and HPKP storage by first party domain (PSM) r=Cykesiopka,keeler
MozReview-Commit-ID: HhFFqtpBNjO

--HG--
extra : rebase_source : 980dfd035efc4886a7ca393923c2c2783cc76a7d

security/manager/ssl/PublicKeyPinningService.cpp
@@ -164,6 +164,7 @@ PublicKeyPinningService::ChainMatchesPinset(const UniqueCERTCertList& certList,
 // Dynamic pins are prioritized over static pins.
 static nsresult
 FindPinningInformation(const char* hostname, mozilla::pkix::Time time,
+                       const OriginAttributes& originAttributes,
                /*out*/ nsTArray<nsCString>& dynamicFingerprints,
                /*out*/ const TransportSecurityPreload*& staticFingerprints)
 {
@@ -190,7 +191,8 @@ FindPinningInformation(const char* hostname, mozilla::pkix::Time time,
     bool includeSubdomains;
     nsTArray<nsCString> pinArray;
     rv = sssService->GetKeyPinsForHostname(nsDependentCString(evalHost), time,
-                                           pinArray, &includeSubdomains, &found);
+                                           originAttributes, pinArray,
+                                           &includeSubdomains, &found);
     if (NS_FAILED(rv)) {
       return rv;
     }
@@ -241,6 +243,7 @@ FindPinningInformation(const char* hostname, mozilla::pkix::Time time,
 static nsresult
 CheckPinsForHostname(const UniqueCERTCertList& certList, const char* hostname,
                      bool enforceTestMode, mozilla::pkix::Time time,
+                     const OriginAttributes& originAttributes,
              /*out*/ bool& chainHasValidPins,
     /*optional out*/ PinningTelemetryInfo* pinningTelemetryInfo)
 {
@@ -254,8 +257,8 @@ CheckPinsForHostname(const UniqueCERTCertList& certList, const char* hostname,
 
   nsTArray<nsCString> dynamicFingerprints;
   const TransportSecurityPreload* staticFingerprints = nullptr;
-  nsresult rv = FindPinningInformation(hostname, time, dynamicFingerprints,
-                                       staticFingerprints);
+  nsresult rv = FindPinningInformation(hostname, time, originAttributes,
+                                       dynamicFingerprints, staticFingerprints);
   // If we have no pinning information, the certificate chain trivially
   // validates with respect to pinning.
   if (dynamicFingerprints.Length() == 0 && !staticFingerprints) {
@@ -326,12 +329,14 @@ CheckPinsForHostname(const UniqueCERTCertList& certList, const char* hostname,
 }
 
 nsresult
-PublicKeyPinningService::ChainHasValidPins(const UniqueCERTCertList& certList,
-                                           const char* hostname,
-                                           mozilla::pkix::Time time,
-                                           bool enforceTestMode,
-                                   /*out*/ bool& chainHasValidPins,
-                          /*optional out*/ PinningTelemetryInfo* pinningTelemetryInfo)
+PublicKeyPinningService::ChainHasValidPins(
+  const UniqueCERTCertList& certList,
+  const char* hostname,
+  mozilla::pkix::Time time,
+  bool enforceTestMode,
+  const OriginAttributes& originAttributes,
+  /*out*/ bool& chainHasValidPins,
+  /*optional out*/ PinningTelemetryInfo* pinningTelemetryInfo)
 {
   chainHasValidPins = false;
   if (!certList) {
@@ -342,14 +347,15 @@ PublicKeyPinningService::ChainHasValidPins(const UniqueCERTCertList& certList,
   }
   nsAutoCString canonicalizedHostname(CanonicalizeHostname(hostname));
   return CheckPinsForHostname(certList, canonicalizedHostname.get(),
-                              enforceTestMode, time, chainHasValidPins,
-                              pinningTelemetryInfo);
+                              enforceTestMode, time, originAttributes,
+                              chainHasValidPins, pinningTelemetryInfo);
 }
 
 nsresult
 PublicKeyPinningService::HostHasPins(const char* hostname,
                                      mozilla::pkix::Time time,
                                      bool enforceTestMode,
+                                     const OriginAttributes& originAttributes,
                                      /*out*/ bool& hostHasPins)
 {
   hostHasPins = false;
@@ -357,7 +363,8 @@ PublicKeyPinningService::HostHasPins(const char* hostname,
   nsTArray<nsCString> dynamicFingerprints;
   const TransportSecurityPreload* staticFingerprints = nullptr;
   nsresult rv = FindPinningInformation(canonicalizedHostname.get(), time,
-                                       dynamicFingerprints, staticFingerprints);
+                                       originAttributes, dynamicFingerprints,
+                                       staticFingerprints);
   if (NS_FAILED(rv)) {
     return rv;
   }

security/manager/ssl/nsSiteSecurityService.h
@@ -5,6 +5,7 @@
 #ifndef __nsSiteSecurityService_h__
 #define __nsSiteSecurityService_h__
 
+#include "mozilla/BasePrincipal.h"
 #include "mozilla/DataStorage.h"
 #include "mozilla/RefPtr.h"
 #include "nsCOMPtr.h"
@@ -17,6 +18,8 @@
 
 class nsIURI;
 class nsISSLStatus;
+
+using mozilla::OriginAttributes;
 
 // {16955eee-6c48-4152-9309-c42a465138a1}
 #define NS_SITE_SECURITY_SERVICE_CID \
@@ -44,6 +47,7 @@ enum SecurityPropertyState {
  * the public key pins of a site.
  * HPKP state consists of:
  *  - Hostname (nsCString)
+ *  - Origin attributes (OriginAttributes)
  *  - Expiry time (PRTime (aka int64_t) in milliseconds)
  *  - A state flag (SecurityPropertyState, default SecurityPropertyUnset)
  *  - An include subdomains flag (bool, default false)
@@ -57,12 +61,16 @@ public:
   NS_DECL_NSISITESECURITYSTATE
 
   SiteHPKPState();
-  SiteHPKPState(const nsCString& aHost, const nsCString& aStateString);
-  SiteHPKPState(const nsCString& aHost, PRTime aExpireTime,
-                SecurityPropertyState aState, bool aIncludeSubdomains,
-                nsTArray<nsCString>& SHA256keys);
+  SiteHPKPState(const nsCString& aHost,
+                const OriginAttributes& aOriginAttributes,
+                const nsCString& aStateString);
+  SiteHPKPState(const nsCString& aHost,
+                const OriginAttributes& aOriginAttributes,
+                PRTime aExpireTime, SecurityPropertyState aState,
+                bool aIncludeSubdomains, nsTArray<nsCString>& SHA256keys);
 
   nsCString mHostname;
+  OriginAttributes mOriginAttributes;
   PRTime mExpireTime;
   SecurityPropertyState mState;
   bool mIncludeSubdomains;
@@ -88,6 +96,7 @@ protected:
  * the security state of a site. Currently only handles HSTS.
  * HSTS state consists of:
  *  - Hostname (nsCString)
+ *  - Origin attributes (OriginAttributes)
  *  - Expiry time (PRTime (aka int64_t) in milliseconds)
  *  - A state flag (SecurityPropertyState, default SecurityPropertyUnset)
  *  - An include subdomains flag (bool, default false)
@@ -99,11 +108,16 @@ public:
   NS_DECL_NSISITEHSTSSTATE
   NS_DECL_NSISITESECURITYSTATE
 
-  SiteHSTSState(const nsCString& aHost, const nsCString& aStateString);
-  SiteHSTSState(const nsCString& aHost, PRTime aHSTSExpireTime,
-                SecurityPropertyState aHSTSState, bool aHSTSIncludeSubdomains);
+  SiteHSTSState(const nsCString& aHost,
+                const OriginAttributes& aOriginAttributes,
+                const nsCString& aStateString);
+  SiteHSTSState(const nsCString& aHost,
+                const OriginAttributes& aOriginAttributes,
+                PRTime aHSTSExpireTime, SecurityPropertyState aHSTSState,
+                bool aHSTSIncludeSubdomains);
 
   nsCString mHostname;
+  OriginAttributes mOriginAttributes;
   PRTime mHSTSExpireTime;
   SecurityPropertyState mHSTSState;
   bool mHSTSIncludeSubdomains;
@@ -150,30 +164,42 @@ private:
   nsresult GetHost(nsIURI *aURI, nsACString &aResult);
   nsresult SetHSTSState(uint32_t aType, const char* aHost, int64_t maxage,
                         bool includeSubdomains, uint32_t flags,
-                        SecurityPropertyState aHSTSState, bool aIsPreload);
+                        SecurityPropertyState aHSTSState, bool aIsPreload,
+                        const OriginAttributes& aOriginAttributes);
   nsresult ProcessHeaderInternal(uint32_t aType, nsIURI* aSourceURI,
                                  const nsCString& aHeader,
                                  nsISSLStatus* aSSLStatus,
-                                 uint32_t aFlags, uint64_t* aMaxAge,
-                                 bool* aIncludeSubdomains,
+                                 uint32_t aFlags,
+                                 const OriginAttributes& aOriginAttributes,
+                                 uint64_t* aMaxAge, bool* aIncludeSubdomains,
                                  uint32_t* aFailureResult);
   nsresult ProcessSTSHeader(nsIURI* aSourceURI, const nsCString& aHeader,
-                            uint32_t flags, uint64_t* aMaxAge,
-                            bool* aIncludeSubdomains, uint32_t* aFailureResult);
+                            uint32_t flags,
+                            const OriginAttributes& aOriginAttributes,
+                            uint64_t* aMaxAge, bool* aIncludeSubdomains,
+                            uint32_t* aFailureResult);
   nsresult ProcessPKPHeader(nsIURI* aSourceURI, const nsCString& aHeader,
                             nsISSLStatus* aSSLStatus, uint32_t flags,
+                            const OriginAttributes& aOriginAttributes,
                             uint64_t* aMaxAge, bool* aIncludeSubdomains,
                             uint32_t* aFailureResult);
   nsresult SetHPKPState(const char* aHost, SiteHPKPState& entry, uint32_t flags,
-                        bool aIsPreload);
+                        bool aIsPreload,
+                        const OriginAttributes& aOriginAttributes);
+  nsresult RemoveStateInternal(uint32_t aType, nsIURI* aURI, uint32_t aFlags,
+                               const OriginAttributes& aOriginAttributes);
   nsresult RemoveStateInternal(uint32_t aType, const nsAutoCString& aHost,
-                               uint32_t aFlags, bool aIsPreload);
+                               uint32_t aFlags, bool aIsPreload,
+                               const OriginAttributes& aOriginAttributes);
   bool HostHasHSTSEntry(const nsAutoCString& aHost,
                         bool aRequireIncludeSubdomains, uint32_t aFlags,
+                        const OriginAttributes& aOriginAttributes,
                         bool* aResult, bool* aCached);
   const nsSTSPreload *GetPreloadListEntry(const char *aHost);
   nsresult IsSecureHost(uint32_t aType, const nsACString& aHost,
-                        uint32_t aFlags, bool* aCached, bool* aResult);
+                        uint32_t aFlags,
+                        const OriginAttributes& aOriginAttributes,
+                        bool* aCached, bool* aResult);
 
   uint64_t mMaxMaxAge;
   bool mUsePreloadList;

arthuredelstein/tor-browser

security/manager/ssl/PublicKeyPinningService.cpp
@@ -164,6 +164,7 @@ PublicKeyPinningService::ChainMatchesPinset(const UniqueCERTCertList& certList,
 // Dynamic pins are prioritized over static pins.
 static nsresult
 FindPinningInformation(const char* hostname, mozilla::pkix::Time time,
+                       const NeckoOriginAttributes& originAttributes,
                /*out*/ nsTArray<nsCString>& dynamicFingerprints,
                /*out*/ const TransportSecurityPreload*& staticFingerprints)
 {
@@ -190,7 +191,8 @@ FindPinningInformation(const char* hostname, mozilla::pkix::Time time,
     bool includeSubdomains;
     nsTArray<nsCString> pinArray;
     rv = sssService->GetKeyPinsForHostname(nsDependentCString(evalHost), time,
-                                           pinArray, &includeSubdomains, &found);
+                                           originAttributes, pinArray,
+                                           &includeSubdomains, &found);
     if (NS_FAILED(rv)) {
       return rv;
     }
@@ -241,6 +243,7 @@ FindPinningInformation(const char* hostname, mozilla::pkix::Time time,
 static nsresult
 CheckPinsForHostname(const UniqueCERTCertList& certList, const char* hostname,
                      bool enforceTestMode, mozilla::pkix::Time time,
+                     const NeckoOriginAttributes& originAttributes,
              /*out*/ bool& chainHasValidPins,
     /*optional out*/ PinningTelemetryInfo* pinningTelemetryInfo)
 {
@@ -254,8 +257,8 @@ CheckPinsForHostname(const UniqueCERTCertList& certList, const char* hostname,
 
   nsTArray<nsCString> dynamicFingerprints;
   const TransportSecurityPreload* staticFingerprints = nullptr;
-  nsresult rv = FindPinningInformation(hostname, time, dynamicFingerprints,
-                                       staticFingerprints);
+  nsresult rv = FindPinningInformation(hostname, time, originAttributes,
+                                       dynamicFingerprints, staticFingerprints);
   // If we have no pinning information, the certificate chain trivially
   // validates with respect to pinning.
   if (dynamicFingerprints.Length() == 0 && !staticFingerprints) {
@@ -326,12 +329,14 @@ CheckPinsForHostname(const UniqueCERTCertList& certList, const char* hostname,
 }
 
 nsresult
-PublicKeyPinningService::ChainHasValidPins(const UniqueCERTCertList& certList,
-                                           const char* hostname,
-                                           mozilla::pkix::Time time,
-                                           bool enforceTestMode,
-                                   /*out*/ bool& chainHasValidPins,
-                          /*optional out*/ PinningTelemetryInfo* pinningTelemetryInfo)
+PublicKeyPinningService::ChainHasValidPins(
+  const UniqueCERTCertList& certList,
+  const char* hostname,
+  mozilla::pkix::Time time,
+  bool enforceTestMode,
+  const NeckoOriginAttributes& originAttributes,
+  /*out*/ bool& chainHasValidPins,
+  /*optional out*/ PinningTelemetryInfo* pinningTelemetryInfo)
 {
   chainHasValidPins = false;
   if (!certList) {
@@ -342,14 +347,15 @@ PublicKeyPinningService::ChainHasValidPins(const UniqueCERTCertList& certList,
   }
   nsAutoCString canonicalizedHostname(CanonicalizeHostname(hostname));
   return CheckPinsForHostname(certList, canonicalizedHostname.get(),
-                              enforceTestMode, time, chainHasValidPins,
-                              pinningTelemetryInfo);
+                              enforceTestMode, time, originAttributes,
+                              chainHasValidPins, pinningTelemetryInfo);
 }
 
 nsresult
 PublicKeyPinningService::HostHasPins(const char* hostname,
                                      mozilla::pkix::Time time,
                                      bool enforceTestMode,
+                                     const NeckoOriginAttributes& originAttributes,
                                      /*out*/ bool& hostHasPins)
 {
   hostHasPins = false;
@@ -357,7 +363,8 @@ PublicKeyPinningService::HostHasPins(const char* hostname,
   nsTArray<nsCString> dynamicFingerprints;
   const TransportSecurityPreload* staticFingerprints = nullptr;
   nsresult rv = FindPinningInformation(canonicalizedHostname.get(), time,
-                                       dynamicFingerprints, staticFingerprints);
+                                       originAttributes, dynamicFingerprints,
+                                       staticFingerprints);
   if (NS_FAILED(rv)) {
     return rv;
   }

security/manager/ssl/nsSiteSecurityService.h
@@ -5,6 +5,7 @@
 #ifndef __nsSiteSecurityService_h__
 #define __nsSiteSecurityService_h__
 
+#include "mozilla/BasePrincipal.h"
 #include "mozilla/DataStorage.h"
 #include "mozilla/RefPtr.h"
 #include "nsCOMPtr.h"
@@ -17,6 +18,8 @@
 
 class nsIURI;
 class nsISSLStatus;
+
+using mozilla::NeckoOriginAttributes;
 
 // {16955eee-6c48-4152-9309-c42a465138a1}
 #define NS_SITE_SECURITY_SERVICE_CID \
@@ -44,6 +47,7 @@ enum SecurityPropertyState {
  * the public key pins of a site.
  * HPKP state consists of:
  *  - Hostname (nsCString)
+ *  - Origin attributes (NeckoOriginAttributes)
  *  - Expiry time (PRTime (aka int64_t) in milliseconds)
  *  - A state flag (SecurityPropertyState, default SecurityPropertyUnset)
  *  - An include subdomains flag (bool, default false)
@@ -57,12 +61,16 @@ public:
   NS_DECL_NSISITESECURITYSTATE
 
   SiteHPKPState();
-  SiteHPKPState(const nsCString& aHost, const nsCString& aStateString);
-  SiteHPKPState(const nsCString& aHost, PRTime aExpireTime,
-                SecurityPropertyState aState, bool aIncludeSubdomains,
-                nsTArray<nsCString>& SHA256keys);
+  SiteHPKPState(const nsCString& aHost,
+                const NeckoOriginAttributes& aOriginAttributes,
+                const nsCString& aStateString);
+  SiteHPKPState(const nsCString& aHost,
+                const NeckoOriginAttributes& aOriginAttributes,
+                PRTime aExpireTime, SecurityPropertyState aState,
+                bool aIncludeSubdomains, nsTArray<nsCString>& SHA256keys);
 
   nsCString mHostname;
+  NeckoOriginAttributes mOriginAttributes;
   PRTime mExpireTime;
   SecurityPropertyState mState;
   bool mIncludeSubdomains;
@@ -88,6 +96,7 @@ protected:
  * the security state of a site. Currently only handles HSTS.
  * HSTS state consists of:
  *  - Hostname (nsCString)
+ *  - Origin attributes (NeckoOriginAttributes)
  *  - Expiry time (PRTime (aka int64_t) in milliseconds)
  *  - A state flag (SecurityPropertyState, default SecurityPropertyUnset)
  *  - An include subdomains flag (bool, default false)
@@ -99,11 +108,16 @@ public:
   NS_DECL_NSISITEHSTSSTATE
   NS_DECL_NSISITESECURITYSTATE
 
-  SiteHSTSState(const nsCString& aHost, const nsCString& aStateString);
-  SiteHSTSState(const nsCString& aHost, PRTime aHSTSExpireTime,
-                SecurityPropertyState aHSTSState, bool aHSTSIncludeSubdomains);
+  SiteHSTSState(const nsCString& aHost,
+                const NeckoOriginAttributes& aOriginAttributes,
+                const nsCString& aStateString);
+  SiteHSTSState(const nsCString& aHost,
+                const NeckoOriginAttributes& aOriginAttributes,
+                PRTime aHSTSExpireTime, SecurityPropertyState aHSTSState,
+                bool aHSTSIncludeSubdomains);
 
   nsCString mHostname;
+  NeckoOriginAttributes mOriginAttributes;
   PRTime mHSTSExpireTime;
   SecurityPropertyState mHSTSState;
   bool mHSTSIncludeSubdomains;
@@ -150,30 +164,42 @@ private:
   nsresult GetHost(nsIURI *aURI, nsACString &aResult);
   nsresult SetHSTSState(uint32_t aType, const char* aHost, int64_t maxage,
                         bool includeSubdomains, uint32_t flags,
-                        SecurityPropertyState aHSTSState, bool aIsPreload);
+                        SecurityPropertyState aHSTSState, bool aIsPreload,
+                        const NeckoOriginAttributes& aOriginAttributes);
   nsresult ProcessHeaderInternal(uint32_t aType, nsIURI* aSourceURI,
                                  const nsCString& aHeader,
                                  nsISSLStatus* aSSLStatus,
-                                 uint32_t aFlags, uint64_t* aMaxAge,
-                                 bool* aIncludeSubdomains,
+                                 uint32_t aFlags,
+                                 const NeckoOriginAttributes& aOriginAttributes,
+                                 uint64_t* aMaxAge, bool* aIncludeSubdomains,
                                  uint32_t* aFailureResult);
   nsresult ProcessSTSHeader(nsIURI* aSourceURI, const nsCString& aHeader,
-                            uint32_t flags, uint64_t* aMaxAge,
-                            bool* aIncludeSubdomains, uint32_t* aFailureResult);
+                            uint32_t flags,
+                            const NeckoOriginAttributes& aOriginAttributes,
+                            uint64_t* aMaxAge, bool* aIncludeSubdomains,
+                            uint32_t* aFailureResult);
   nsresult ProcessPKPHeader(nsIURI* aSourceURI, const nsCString& aHeader,
                             nsISSLStatus* aSSLStatus, uint32_t flags,
+                            const NeckoOriginAttributes& aOriginAttributes,
                             uint64_t* aMaxAge, bool* aIncludeSubdomains,
                             uint32_t* aFailureResult);
   nsresult SetHPKPState(const char* aHost, SiteHPKPState& entry, uint32_t flags,
-                        bool aIsPreload);
+                        bool aIsPreload,
+                        const NeckoOriginAttributes& aOriginAttributes);
+  nsresult RemoveStateInternal(uint32_t aType, nsIURI* aURI, uint32_t aFlags,
+                               const NeckoOriginAttributes& aOriginAttributes);
   nsresult RemoveStateInternal(uint32_t aType, const nsAutoCString& aHost,
-                               uint32_t aFlags, bool aIsPreload);
+                               uint32_t aFlags, bool aIsPreload,
+                               const NeckoOriginAttributes& aOriginAttributes);
   bool HostHasHSTSEntry(const nsAutoCString& aHost,
                         bool aRequireIncludeSubdomains, uint32_t aFlags,
+                        const NeckoOriginAttributes& aOriginAttributes,
                         bool* aResult, bool* aCached);
   const nsSTSPreload *GetPreloadListEntry(const char *aHost);
   nsresult IsSecureHost(uint32_t aType, const nsACString& aHost,
-                        uint32_t aFlags, bool* aCached, bool* aResult);
+                        uint32_t aFlags,
+                        const NeckoOriginAttributes& aOriginAttributes,
+                        bool* aCached, bool* aResult);
 
   uint64_t mMaxMaxAge;
   bool mUsePreloadList;