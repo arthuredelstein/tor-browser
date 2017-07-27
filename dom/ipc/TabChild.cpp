42124d979dca1e40d4a685ccb07249852a800c6f
Arthur Edelstein <arthuredelstein@gmail.com>
2017-07-27 15:27:54 -0700

    Fix ubsan runtime error for nsEventStatus enum
    Fixes the following error:
    gfx/layers/apz/util/InputAPZContext.cpp:58:30: runtime error: load of value 4142693320, which is not a valid value for type 'nsEventStatus'

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -1681,8 +1681,7 @@ TabChild::RecvRealMouseButtonEvent(const WidgetMouseEvent& aEvent,
                                                         aInputBlockId);
   }
 
-  nsEventStatus unused;
-  InputAPZContext context(aGuid, aInputBlockId, unused);
+  InputAPZContext context(aGuid, aInputBlockId, nsEventStatus_eUndefined);
   if (pendingLayerization) {
     context.SetPendingLayerization();
   }
