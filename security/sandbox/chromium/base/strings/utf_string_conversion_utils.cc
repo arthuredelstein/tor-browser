73012b3859743dc7324dfff992ac42ade4f9fae2
Georg Koppen <gk@torproject.org>
2017-08-11 08:08:39 +0000

    Bug 16010: Fixing sandbox compile issues

--- security/sandbox/chromium/base/strings/utf_string_conversion_utils.cc
+++ security/sandbox/chromium/base/strings/utf_string_conversion_utils.cc
@@ -122,7 +122,7 @@ void PrepareForUTF8Output(const CHAR* src,
 
 // Instantiate versions we know callers will need.
 template void PrepareForUTF8Output(const wchar_t*, size_t, std::string*);
-template void PrepareForUTF8Output(const char16*, size_t, std::string*);
+//template void PrepareForUTF8Output(const char16*, size_t, std::string*);
 
 template<typename STRING>
 void PrepareForUTF16Or32Output(const char* src,
@@ -143,6 +143,6 @@ void PrepareForUTF16Or32Output(const char* src,
 
 // Instantiate versions we know callers will need.
 template void PrepareForUTF16Or32Output(const char*, size_t, std::wstring*);
-template void PrepareForUTF16Or32Output(const char*, size_t, string16*);
+//template void PrepareForUTF16Or32Output(const char*, size_t, string16*);
 
 }  // namespace base
